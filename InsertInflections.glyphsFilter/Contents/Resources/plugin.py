6ac09ba3f49878029a3d32e7b29934a45fb823c7
Rainer Scheichelbauer <res@glyphsapp.com>
2020-04-11 18:30:34 +0200

    Glyphs 3 compatibility

--- InsertInflections.glyphsFilter/Contents/Resources/plugin.py
+++ InsertInflections.glyphsFilter/Contents/Resources/plugin.py
@@ -1,4 +1,16 @@
 # encoding: utf-8
+from __future__ import division, print_function, unicode_literals
+
+###########################################################################################################
+#
+#
+#	Filter without dialog Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20without%20Dialog
+#
+#
+###########################################################################################################
 
 import objc
 from GlyphsApp import *
@@ -6,6 +18,7 @@ from GlyphsApp.plugins import *
 
 class InsertInflections(FilterWithoutDialog):
 	
+	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
 			'en': u'Insert Inflections',
@@ -16,9 +29,11 @@ class InsertInflections(FilterWithoutDialog):
 		})
 		self.keyboardShortcut = None # With Cmd+Shift
 
+	@objc.python_method
 	def filter(self, thisLayer, inEditView, customParameters):
 		thisLayer.addInflectionPoints()
 		
+	@objc.python_method
 	def __file__(self):
 		"""Please leave this method unchanged"""
 		return __file__
